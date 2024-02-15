e1302c111bc8b63e77ea219af8e0f4ddff9d5c45
Mzwa1080 <mshawnlouw@gmail.com>
2024-02-15 16:15:35 +0200

    added controller and middlewares

---
+++ controller/ProductController.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { products  } from '../model/index'
+import { verifyToken } from '../middleware/AuthenticateUser'
+
+const productRouter = express.Router()
+
+// Fetch users
+productRouter.get('/', (req,res)=>{
+    try{
+        products.fetchProducts(req, res)
+    }catch(e){
+        res.json({
+            status : res.statusCode,
+            msg : 'Failed to retrieve users'
+        })
+    }
+})
+// fetch single user
+productRouter.get('/:id', (req,res)=>{
+    try {
+        products.fetchProduct(req,res)
+    }catch(e){
+        res.json({
+            status:res.statusCode,
+            msg : 'Failed to retrieve a user'
+        })
+    }
+})
+// add  a user
+
+productRouter.post('/register', bodyParser.json(), (req,res)=>{
+    try {
+        products.addProduct(req,res)
+    } catch (err) {
+        res.json({
+            status : res.statusCode,
+            msg : 'Failed to add a new user.'
+        })
+    }
+})
+
+export {
+    productRouter, express
+}
